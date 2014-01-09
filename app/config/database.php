054d7c90211d63d3edc4629ad5f2b968b1f46886
Ezra Sharp <ezra@kiwidev.co.nz>
2014-01-10 03:56:03 +1300

    Fix db config name

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => Config::get('Laravel.database.db_name'),
-			'username'  => Config::get('Laravel.database.username'),
-			'password'  => Config::get('Laravel.database.password'),
+			'database'  => Config::get('laravel.database.db_name'),
+			'username'  => Config::get('laravel.database.username'),
+			'password'  => Config::get('laravel.database.password'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
