d3abe3f5b5e604404f37205d65360a8f7ebe7d4b
Ezra Sharp <ezra@kiwidev.co.nz>
2014-01-18 12:20:45 +1300

    Add Directions Routing

--- app/views/information.blade.php
+++ app/views/information.blade.php
@@ -21,6 +21,106 @@
 		<p>If you have questions about getting there, see the transport questions page or get directions from Google Maps here.</p>
 		<p>Eastercamp is for <strong>youth group aged young people and their leaders</strong>. General Campers must be <strong>aged between 13-18 years old</strong> at the start of camp or be in Year 9-13 at school in 2014.</p>
 	</div>
+
+	<div class="small-12 columns">
+
+	    <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	    <script>
+	      function calculateRoute(from, to) {
+	        // Center initialized to Naples, Italy
+	        var myOptions = {
+	          zoom: 10,
+	          center: new google.maps.LatLng(-43.429944, 172.705426),
+	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	        };
+	        // Draw the map
+	        var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+	 
+	        var directionsService = new google.maps.DirectionsService();
+	        var directionsRequest = {
+	          origin: from,
+	          destination: to,
+	          travelMode: google.maps.DirectionsTravelMode.DRIVING,
+	          unitSystem: google.maps.UnitSystem.METRIC
+	        };
+	        directionsService.route(
+	          directionsRequest,
+	          function(response, status)
+	          {
+	            if (status == google.maps.DirectionsStatus.OK)
+	            {
+	              new google.maps.DirectionsRenderer({
+	                map: mapObject,
+	                directions: response
+	              });
+	            }
+	            else
+	              $("#error").append("Unable to retrieve your route<br />");
+	          }
+	        );
+	      }
+	 
+	      $(document).ready(function() {
+	        // If the browser supports the Geolocation API
+	        if (typeof navigator.geolocation == "undefined") {
+	          $("#error").text("Your browser doesn't support the Geolocation API");
+	          return;
+	        }
+	 
+	        $("#from-link, #to-link").click(function(event) {
+	          event.preventDefault();
+	          var addressId = this.id.substring(0, this.id.indexOf("-"));
+	 
+	          navigator.geolocation.getCurrentPosition(function(position) {
+	            var geocoder = new google.maps.Geocoder();
+	            geocoder.geocode({
+	              "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+	            },
+	            function(results, status) {
+	              if (status == google.maps.GeocoderStatus.OK)
+	                $("#" + addressId).val(results[0].formatted_address);
+	              else
+	                $("#error").append("Unable to retrieve your address<br />");
+	            });
+	          },
+	          function(positionError){
+	            $("#error").append("Error: " + positionError.message + "<br />");
+	          },
+	          {
+	            enableHighAccuracy: true,
+	            timeout: 10 * 1000 // 10 seconds
+	          });
+	        });
+	 
+	        $("#calculate-route").submit(function(event) {
+	          event.preventDefault();
+	          calculateRoute($("#from").val(), '110 Heyders Road, Bottle Lake, Christchurch 8083, New Zealand');
+	        });
+	      });
+	    </script>
+	    <style type="text/css">
+	      #map {
+	        width: 500px;
+	        height: 400px;
+	        margin-top: 10px;
+	      }
+	    </style>
+
+	    <h1>Calculate your route</h1>
+	    <form id="calculate-route" name="calculate-route" action="#" method="get">
+	      <label for="from">From:</label>
+	      <input type="text" id="from" name="from" required="required" placeholder="An address" size="30" />
+	      <a id="from-link" href="#">Get my position</a>
+	      <br />
+	 
+	      <input type="submit" />
+	      <input type="reset" />
+	    </form>
+	    <div id="map"></div>
+	    <p id="error"></p>
+
+	</div>
 </div>
 
 @stop
