193c7f3a6d08dd48ad890a23a1b3123299ab4da4
Ezra Sharp <ezra@kiwidev.co.nz>
2014-02-06 22:32:27 +1300

    Fix thumbnail height again

--- app/views/playlists/public.blade.php
+++ app/views/playlists/public.blade.php
@@ -7,7 +7,7 @@
 		<ul class="small-block-grid-1 medium-block-grid-3 large-block-grid-4">
 			@foreach ($playlist->videos as $video)
 			@if($video->public > 0)
-			<li><a class="fancybox overlay-icon" title="{{ $video->title }}" href="http://www.youtube.com/watch?v={{ $video->url }}"><img class="th" src="https://i.embed.ly/1/display/crop?key=d5a004fad9d94741b9ea438a9b802b3e&url=http://i.ytimg.com/vi/{{ $video->url }}/hqdefault.jpg?v=1&height=170&width=300"><i class="fa fa-play-circle"></i></a><p class="video-title">{{ $video->title }}</p></li>
+			<li><a class="fancybox overlay-icon" title="{{ $video->title }}" href="http://www.youtube.com/watch?v={{ $video->url }}"><img class="th" src="https://i.embed.ly/1/display/crop?key=d5a004fad9d94741b9ea438a9b802b3e&url=http://i.ytimg.com/vi/{{ $video->url }}/hqdefault.jpg&height=168&width=300"><i class="fa fa-play-circle"></i></a><p class="video-title">{{ $video->title }}</p></li>
 			@endif
 			@endforeach
 		</ul>
